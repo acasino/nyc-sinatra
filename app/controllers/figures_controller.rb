61075bc924d14a17fbe4697a9d4bccec561e3866
Andrew <andrew.casin@gmail.com>
2021-01-31 19:00:07 -0600

    commit tree 0fee6a8144a2328062752f63146b4853cc3bb0a9

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,64 @@
 class FiguresController < ApplicationController
   # add controller methods
+
+  #GET: Index Action - index page to display all figures
+  get '/figures' do
+    @figures = Figure.all 
+    erb :'figures/index'
+  end
+
+  #GET: New Action - displays create figure form
+  get '/figures/new' do
+    erb :'figures/new'
+  end
+
+  #GET: Show Action - displays one figure based on ID in the URL
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
+
+  #GET: Edit Action - displays edit form based on ID in the URL
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/edit'
+  end
+
+  #POST: Create Action - creates one more figure
+  post '/figures' do
+    @figure = Figure.create(params['figure'])
+    unless params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    unless params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
+
+
+  #PATCH: Update Action - modifies an existing article based on ID in the URL
+  patch '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    @figure.update(params[:figure])
+    unless params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+    unless params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
+
+  #PUT: Update Action - replaces an existing article based on ID in the URL
+  ##Not used in this lab
+
+  #DELETE: Delete Action = delte one article based on ID in URL
+  ##Not used in this lab
+
+
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,42 @@
 class LandmarksController < ApplicationController
   # add controller methods
+  #GET: Index Action
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    @figures = Figure.all
+    erb :'landmarks/index'
+  end
+
+  #GET: New Action
+  get '/landmarks/new' do
+    erb :"landmarks/new"
+  end
+
+  #GET: Show Action
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :"landmarks/show"
+  end
+
+  #GET: Edit Action
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :"landmarks/edit"
+  end
+
+  #GET: Update Action
+  post '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.name = params['landmark']['name']
+    @landmark.year_completed = params['landmark']['year_completed']
+    @landmark.save
+    redirect to "/landmarks/#{@landmark.id}"
+  end
+
+  #POST: Create Action
+  post '/landmarks' do
+    Landmark.create(name: params['landmark']['name'], year_completed: params['landmark']['year_completed'])
+    redirect '/landmarks'
+  end
+
 end
